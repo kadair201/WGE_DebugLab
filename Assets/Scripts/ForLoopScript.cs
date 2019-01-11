a2ccaa3a622ec3567169d8ff9f36dc90d35cb932
KAGSme <kags1995@hotmail.co.uk>
2019-01-11 16:50:49 +0000

    Adding unity project

---
+++ Assets/Scripts/ForLoopScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoopScript : MonoBehaviour {
+
+    // start for loop 
+    public void ExecuteLoop()
+    {
+        int x = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            x += i;
+        }
+    }
+}
---
+++ Assets/Scripts/LerpCubeScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpCubeScript : MonoBehaviour {
+
+    public GameObject _cube;
+    public Vector3 _leftPosition;
+    public Vector3 _rightPosition;
+
+    public void StartLerp()
+    {
+        _cube.transform.position = _leftPosition;
+        StartCoroutine(LerpCube());
+    }
+
+    IEnumerator LerpCube()
+    {
+        float t = 0;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            Debug.Log(t);
+            _cube.transform.position = Vector3.Lerp(_leftPosition, _rightPosition, t);
+            if(t >=1)
+            {
+                _cube.transform.position = _rightPosition;
+            }
+            yield return null;
+        }
+    }
+
+    //inseert code here:
+
+}
